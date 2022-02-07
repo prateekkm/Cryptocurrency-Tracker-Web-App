fda5a25016de4ecdac214f6e58ca9e724ab43f65
PRATEEK KUMAR MAHAPATRA <66913654+prateekkm@users.noreply.github.com>
2022-02-07 16:08:58 +0530

    Changed the API

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('#searchForm');
 const res = document.querySelector('#tableResult');
-const cont = document.getElementById("allContaint");
 var upd;
 
 form.addEventListener('submit', (e)=>{
@@ -9,34 +8,19 @@ form.addEventListener('submit', (e)=>{
         clearTimeout(upd);
     }
     const ctype = form.elements.coinType.value;
-    cont.classList.add('mainClick');
-    cont.classList.remove('main'); 
     fetchPrice(ctype);
 
 });
 
-function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    return time;
-  }
-  console.log(timeConverter(0));
 
 const fetchPrice = async(ctype) =>{
-    const r = await axios.get(`https://api.cryptonator.com/api/ticker/${ctype}`);
-    const price = r.data.ticker.price;
-    const volume = r.data.ticker.volume;
-    const change = r.data.ticker.change;
-    const base = r.data.ticker.base;
-    const target = r.data.ticker.target;
-    const time = timeConverter(r.data.timestamp);
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=INR`);
+    console.log(r.data.coin.price);
+    const price = r.data.coin.price;
+    const volume  = r.data.coin.volume;
+    const change = r.data.coin.priceChange1d;
+    const base = r.data.coin.name;
+    const target = 'INR';
     var col = "green";
     if(change < 0){
         col = "red";
@@ -51,7 +35,7 @@ const fetchPrice = async(ctype) =>{
 </tr>
 <tr>
     <td>
-        ${base}
+        Price
     </td>
     <td style="color:${col};"><span style="font-size: 1.3em;">${price}</span> ${target}</td>
 </tr>
@@ -66,14 +50,6 @@ const fetchPrice = async(ctype) =>{
         Change (24hrs)
     </td>
     <td style="color:${col};">${change} ${target}</td>
-</tr>
-<tr>
-    <td>
-        Last Update
-    </td>
-    <td>
-        ${time}
-    </td>
 </tr>`;
 
     upd = setTimeout(()=>fetchPrice(ctype), 10000);
